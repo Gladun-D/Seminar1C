033b32f7370ecce73264e3b898c2f8afb8bcd7ce
Dmitry <gladu@yandex.ru>
2022-11-21 18:26:50 +0300

    Решение задачи 2

---
+++ Lesson01/Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int A = 0;
+int B = 28;
+int C = 7;
+int max = A;
+if (max > B ) max = A;
+if (B > max ) max = B;
+if (C > max ) max = C;
+Console.Write ("max= ");
+Console.WriteLine(max);
