0be38e3183cbdcb5ccb60173404a7401b862d2a9
Dmitry <gladu@yandex.ru>
2022-11-23 19:24:53 +0300

    Семинар 2 Задача 10

---
+++ Lesson01/Task04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.8 -> 2, 4, 6, 8
+
+int N = 8;
+int currentValue = 1;
+while (currentValue <= N)
+{
+    if (currentValue % 2 == 0)
+    {
+        Console.Write(currentValue + " ");
+    }
+currentValue++;
+}
+
