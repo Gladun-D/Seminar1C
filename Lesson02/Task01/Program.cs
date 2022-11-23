34454d6e3e960a43edc277df00f01da54943511b
Dmitry <gladu@yandex.ru>
2022-11-24 01:42:24 +0300

    1 задача 2 семинара

---
+++ Lesson02/Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 д
+Console.WriteLine("Введите трёхзначное число");
+string number = Console.ReadLine();
+int stringlenght = 3;
+int numberlenght = number.Length;
+while(true)
+{
+    if(numberlenght == stringlenght)
+    Console.WriteLine("{0}->{1}",number, number[1]);
+    break;
+}
+
+
+
+
+
+
+
