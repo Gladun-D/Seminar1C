41b14f127a58f3a3fbd00e0cf2b8eb823e069040
Dmitry <gladu@yandex.ru>
2022-11-24 01:11:41 +0300

    1 задача второго семинара

--- Seminar/Program.cs
+++ Seminar/Program.cs
@@ -1,11 +1,12 @@
 ﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 д
-static void Main(string[] args)
-int n = int.Parse(1);
-int m = int.Parse(8);
-for (int i = n; i <= m; i++)
-{
-  if (i % 2 == 0)
-  {
-  Console.Write(i + " ");
-  }
-}
+Console.WriteLine("Введите трехзначное число: ");
+string input = Console.ReadLine();
+int stringlen = 3;
+int inputlen = input.Length;
+    while(true)
+    {
+        if(inputlen == stringlen)
+            Console.WriteLine("{0}->{1}",input, input[1]);
+            break;
+    }
+    
