2dcdbb3aae1ef65618dfb0a56a7e8f94f89836a0
Dmitry <gladu@yandex.ru>
2022-11-24 19:14:08 +0300

    Блок схема 2 задачи

---
+++ Lesson02/Task02/Program.cs
@@ -0,0 +1,14 @@
+﻿// // Напишите программу, которая выводит третью цифру 
+//заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int numberlenght = number.Length;
+int index = 2;
+if (numberlenght >= index + 1)
+{
+    Console.WriteLine("{0}->{1}",number, number[index]);
+}
+else 
+{
+   Console.WriteLine("Третей цифры нет");
+}
