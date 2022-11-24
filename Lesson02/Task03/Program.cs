9000b9a67fd84eadc0d719236e81b0ba5a4af5df
Dmitry <gladu@yandex.ru>
2022-11-24 23:04:51 +0300

    Блок дз 3

---
+++ Lesson02/Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+int dayNumber = 5;
+if (1<= dayNumber & dayNumber <= 5)
+{
+    Console.WriteLine("Будний День");
+}
+if (6<= dayNumber & dayNumber <= 7)
+{
+    Console.WriteLine("Ура - Выходной");
+}
+if (dayNumber > 7)
+{
+    Console.WriteLine("Не корректное число");
+}
