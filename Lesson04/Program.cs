c77218a336e92651964f278a383935d91ea1d681
Dmitry <gladu@yandex.ru>
2022-11-25 18:59:42 +0300

    Задача 1 Семинар

---
+++ Lesson04/Program.cs
@@ -0,0 +1,28 @@
+﻿//  Напишите метод, который принимает на вход два числа (A и B)
+ 
+//возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите первое число: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int B = int.Parse(Console.ReadLine());
+int raisedNumber = A;
+for (int index = 1; index < B; index++)
+{
+    raisedNumber = raisedNumber * A;
+}
+Console.WriteLine("A в степени B: " + raisedNumber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
