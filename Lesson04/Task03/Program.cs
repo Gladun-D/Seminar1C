fe3e6718db6ec1c488d201f77460a8f6d8310321
Dmitry <gladu@yandex.ru>
2022-11-29 03:47:28 +0300

    Задач 3\ Семинар Решен 4

---
+++ Lesson04/Task03/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Напишите методы, который задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Write("Введите любое число эллементов массива: ");
+int enterNumber = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[enterNumber];
+for (int i = 0; i < enterNumber; i++)
+{
+  array[i] = new Random().Next(enterNumber+1);
+  Console.Write(array[i] + ", ");
+}
+Console.WriteLine();
