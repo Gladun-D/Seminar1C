de8da59e02fa08af776bb31191d11bdc6f40f9d1
Dmitry <gladu@yandex.ru>
2022-11-29 03:56:56 +0300

    Задача 3\ Семинар 4 полностью

---
+++ Lesson04/Task02/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//      Метод поиска суммы цифр в числе
+
+int SumNumber(int number)
+{
+    int counter = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+    for (int i = 0; i < counter; i++)
+    {
+        advance = number - number % 10;
+        result = result + (number - advance);
+        number = number / 10;
+    }
+    return result;
+}
+
+int sumNumber = SumNumber(number);
+Console.WriteLine("Сумма" + sumNumber );
+
+
+
+
+
