94beec7eab8cb9962585eece3a91af7474ac79d6
Dmitry <gladu@yandex.ru>
2022-11-30 14:50:50 +0300

    Методы создал

--- Seminar/Task04/Program.cs
+++ Seminar/Task04/Program.cs
@@ -1,10 +1,49 @@
-﻿//Напишите методы, который задаёт массив из 8 элементов и выводит их на экран.
-Console.Write("Введите любое число эллементов массива: ");
-int enterNumber = Convert.ToInt32(Console.ReadLine());
-int[] array = new int[enterNumber];
-for (int i = 0; i < enterNumber; i++)
-{
-  array[i] = new Random().Next(enterNumber+1);
-  Console.Write(array[i] + ", ");
+﻿
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//Метод для создания массива размера size из случайных чисел из диапазона [start;end]
+int[] GenerateRandomArray(int size, int start, int end)
+            {
+                //объявляем массив размера size( количество элементов = size)
+                // это выделена область памяти для переменных типа int (количество переменных = size)
+                int[] RandomArray = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                    //Генерируем чисело из диапазона [start;end] и записываем его в i-йэлемент массива
+                    RandomArray[i] = new Random().Next(start, end + 1);
+                }
+                //так как "int[] Gen" то вернуть должны что-то типа int[]. А это массив СОЗДАННЫЙ
+                return RandomArray;
+            }
+
+            //Метод для вывода элементов массива в консоль для вывода на экрана
+            //передаём "()" в метод ShowArray массив "int[] "
+            void ShowArray(int[] Array1)
+            {
+                for (int i = 0; i < Array1.Length; i++)
+                {
+                    Console.WriteLine(Convert.ToString(Array1[i]));
+                }
+            }
+
+            // Проверка количества чётных чисел (элементов) массива
+            int CountOfEven(int[] myArray2)
+            //передаём "()" в метод ShowArray массив "int[] ", возвращаем число "int Co..."
+            {
+                //Счётчик, куда сохраняем сколько раз попались чётные числа (при каждом новом нахождении +1)
+                int Count = 0;
+                for (int i = 0; i < myArray2.Length; i++)
+                    if (myArray2[i] % 2 == 0)
+                    {
+                        Count = Count + 1;
+                    }
+                return Count;
+            }
+
+            // ВЫЗОВ РАНЕЕ ОБЪЯВЛЕННЫХ МЕТОДОВ
+            int[] myArray = new int[5];
+            myArray = GenerateRandomArray(5, 100, 999);
+            ShowArray(myArray);
+            Console.WriteLine("количество четных чисел массива " + CountOfEven(myArray));
+           
+  }
 }
-Console.WriteLine();
---
+++ Seminar/ViduMetodov/Program.cs
@@ -0,0 +1,45 @@
+﻿// Методы
+// Вид 1 - ничего не возвращают и ничего не принимают.
+void Method1()
+{
+    Console.WriteLine("Автор: Дмитрий Михайлович");
+}
+// Вызов метода следующая строка(Пока её убрали, что бы вызывать следующие методы)
+//Method1(); 
+
+// Вид 2 - ничего не возвращают, но могут принимать какието аргументы(операторов)
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+// Вызов метода следующая строка(Пока её убрали, что бы вызывать следующие методы)
+//Method2(msg: "Текст сообщения", count: 4);
+
+//Вид 4 - Метод который что-то возвращает, но ничего не принимает.
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+// Вызов метода следующая строка(Пока её убрали, что бы вызывать следующие методы)
+// Console.WriteLine(year);
+
+// Вид 4 - Методы которые что-то принимают и что-то возвращают.
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "Женя ты супер!\n");
+Console.WriteLine(res);
