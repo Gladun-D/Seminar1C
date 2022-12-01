fd56ecc1282caffd2fa501b029da4ff490ecaae7
Dmitry <gladu@yandex.ru>
2022-12-02 02:02:49 +0300

    5 семинар

--- Lesson05/Task01/Program.cs
+++ Lesson05/Task01/Program.cs
@@ -1,6 +1,37 @@
 ﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. 
 //  Напишите программу, которая покажет количество чётных чисел в массиве.
 
-Console.Write("Введите любое число эллементов массива: ");
-int enterNumber = Convert.ToInt32(Console.ReadLine());
-int[] array = new int 
+
+int[] CreateArray(int size, int min, int max) 
+{
+    int[] MiArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        MiArray[i] = new Random().Next(min, max);
+    }
+    return MiArray;
+}
+
+void PrintArray(int[] Array1)
+{
+    for (int i = 0; i < Array1.Length; i++)
+    {
+        Console.Write(Convert.ToString($"{Array1[i]}  "));
+    }
+}
+int CountOfEven(int[] Array2)
+{
+    int count = 0;
+    for (int i = 0; i < Array2.Length; i++)
+    {
+        if (Array2[i] % 2 == 0)
+        {
+            count = count +1;
+        }
+    }
+    return count;
+}
+int [] myArray = new int [10];
+myArray = CreateArray(10, 100, 999);
+PrintArray(myArray);
+Console.WriteLine("Чётные числа" + CountOfEven(myArray));
