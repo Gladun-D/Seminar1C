e70f787e3b4d4eaad05a314b9c319d92ac2a205d
Dmitry <gladu@yandex.ru>
2022-12-02 05:03:48 +0300

    7 семинар

---
+++ Lesson07/Program.cs
@@ -0,0 +1,44 @@
+﻿// *Сделать вывод только нечетных коэффициентов у треугольника распечатайте хотя бы 120 строк
+
+//метод, создающий треугольник Паскаля
+int[,] CreateTriangle(int row)
+{
+  int[,] triangle = new int[row, row];
+  for (int i = 0; i < row; i++)
+  {
+    triangle[i, 0] = 1;
+    triangle[i, i] = 1;
+  }
+
+  for (int i = 2; i < row; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      triangle[i, j] =
+      triangle[i - 1, j - 1] + triangle[i - 1, j];
+    }
+  }
+  return triangle;
+}
+
+//метод печати только нечетных коэффициентов треугольника Паскаля
+void PrintTriangle(int[,] triangle)
+{
+  int row = triangle.GetLength(0);
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < row; j++)
+    {
+        if (triangle[i, j] % 2 != 0)
+            Console.Write("*");
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] tr = CreateTriangle(120); 
+PrintTriangle(tr);
